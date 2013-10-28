595805c53f9c3733705704fdd0716d432b1ccc8c
azochniak <azochniak@mt-silesia.com>
2013-10-28 13:39:49 +0100

    python server allows using /?cmd=serverStart and /?serverStop, but not implemented yet

--- cam.py
+++ cam.py
@@ -15,12 +15,38 @@ class myHandler(SimpleHTTPRequestHandler):
         self.send_header("Last-Modified", self.date_time_string())
       self.end_headers()
       self.wfile.write(content);
+  def serve_content(self,data):
+        self.send_response(200)
+        self.send_header("Content-Length", len(data))
+        self.send_header("Last-Modified", self.date_time_string())
+        self.end_headers()
+        self.wfile.write(data);
+  def serverStart(self): 
+    ret=""
+    ret+="system.os.execute(sudo motion -n);"
+    print ret
+    return ret+"OK"
+  def serverStop(self): 
+    ret=""
+    ret+="system.os.execute(killall motion);"
+    print ret
+    return ret+"OK";
   def do_GET(self): 
-    if len(self.path.split('?',1))==2:
+    if len(self.path.split('?',1))>1:
       arg=self.path.split('?',1)[1]
-      print arg
-      if arg=='lastsnap':
+      args=arg.split('&')
+      cmd=args[0].split('=',1)
+      print "arg=%s args=%s cmd=%s" % (arg, args, cmd)
+      if args[0]=='lastsnap':
         self.serve_file('/tmp/motion/lastsnap.jpg');
+      elif cmd[0]=='cmd':
+        ret=("executing "+cmd[1])
+        fnc=({ #http://stackoverflow.com/a/60211/781312
+        'serverStop': self.serverStop
+        ,'serverStart': self.serverStart
+        }[cmd[1]])
+        ret+=fnc()
+        self.serve_content(ret)
       else: 
         data=arg
         self.send_response(200)
@@ -41,7 +67,7 @@ Protocol     = "HTTP/1.0"
 if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
-    port = 8080
+    port = 8000
 server_address = ('0.0.0.0', port)
 
 HandlerClass.protocol_version = Protocol
